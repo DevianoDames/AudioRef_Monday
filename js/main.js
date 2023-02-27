3018de6e13f33b1622f006c2d0bc1aeace4f27d8
DevianoDames <d_dames183654@fanshaweonline.ca>
2023-02-27 17:38:30 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const albumCovers= document.querySelectorAll("#album-art img"), theAudioEl = doc
 playButton = document.querySelector('#playButton'),
 pauseButton = document.querySelector('#pauseButton'),
 rewindButton = document.querySelector('#rewindButton');
+volumeControl = document.querySelector('#volumeControl')
 
 // load the new audio source
 function loadAudio() {
@@ -23,10 +24,19 @@ function playAudio() { theAudioEl.play(); }
 
 function restartAudio () { theAudioEl.currentTime = 0; }
 function pauseAudio() { theAudioEl.pause (); }
+
+function setVolume() {
+    // get the numeric value of the volume slider between 0 (min) and 100 (max)
+    //that's what the volume of the audio should be set to
+    console.log(this.value)
+
+    //divide the value by 100 to get a floating point number bewtween 0 and 1 -> .5, .85 etc
+    //and then set the audio element's volume level to match
+    theAudioEl.volume = (this.value/100);
+}
+
 // add event handling to the alubum covers ( listen for a click event)
 albumCovers.forEach(cover => cover.addEventListener('click', loadAudio))
-
-
 
 
 // add event handling for the custom controls
@@ -34,4 +44,4 @@ playButton.addEventListener('click', playAudio);
 rewindButton.addEventListener('click', restartAudio);
 pauseButton.addEventListener('click', pauseAudio);
 
-volSlider.addEventListener('change',setVolume);
+volumeControl.addEventListener('change',setVolume);

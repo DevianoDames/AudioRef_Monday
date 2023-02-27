7ae67716a765eea39478fe7307048d9a8751ae06
DevianoDames <d_dames183654@fanshaweonline.ca>
2023-02-27 17:17:15 -0500

    added js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
-// create a module for our project
-(() => {
+// collecr all of the album covers into a variable
+const albumCovers= document.querySelectorAll("#album-art img"), theAudioEl = document.querySelector('audio'),
+playButton = document.querySelector('#playButton'),
+pauseButton = document.querySelector('#pauseButton'),
+rewindButton = document.querySelector('#rewindButton');
 
-})()
+// load the new audio source
+function loadAudio() {
+    let currentSrc = `audio/${this.dataset.trackref}.mp3`;
+   
+    // set the new audio source
+    theAudioEl.src = currentSrc;
+    
+    //load the new audio source
+    theAudioEl.load();
+
+    // tell the audio element to play
+ playAudio();
+       
+}
+// tell the audio element to play 
+function playAudio() { theAudioEl.play(); }
+
+function restartAudio () { theAudioEl.currentTime = 0; }
+function pauseAudio() { theAudioEl.pause (); }
+// add event handling to the alubum covers ( listen for a click event)
+albumCovers.forEach(cover => cover.addEventListener('click', loadAudio))
+
+
+
+
+// add event handling for the custom controls
+playButton.addEventListener('click', playAudio);
+rewindButton.addEventListener('click', restartAudio);
+pauseButton.addEventListener('click', pauseAudio);
+
+volSlider.addEventListener('change',setVolume);
